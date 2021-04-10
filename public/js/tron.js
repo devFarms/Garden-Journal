38e1508c191905ed8be802f7f763d69795abacc9
Alex <alex@devfarms.com>
2021-04-10 05:22:16 +0000

    Added button and onclick to start call to new endpoint.

--- public/js/tron.js
+++ public/js/tron.js
@@ -1,8 +1,14 @@
 const button = document.getElementById('submit');
         button.addEventListener('click', async event => {
+          
+          // remove contents from div id plants
           document.getElementById('plants').innerHTML = "";
+          
+          // create new variable 'plant' and add content from form
           const plant = document.getElementById('plant').value;
           console.log(plant);
+          
+          // send form input to broad search endpoint
           const PLANT_ENDPOINT = '/get/' + plant;
           const response = await fetch(PLANT_ENDPOINT);
           const json = await response.json();
@@ -30,11 +36,31 @@ const button = document.getElementById('submit');
             let plant_family = document.createTextNode(json.data[i].family);
             let plant_genus = document.createTextNode(json.data[i].genus);
 
+            let more_button = document.createElement('button');
+            more_button.className = 'more-button';
+            more_button.onclick = function() { plantDetail() };
+            //more_button.textContent = 'More Info';
+            more_button.textContent = json.data[i].links.self;
+            more_info_link = json.data[i].links.self;      
+
             document.getElementById('plants').appendChild(div_plant).appendChild(div_plant_image).appendChild(img_src);
             document.getElementById('plants').appendChild(div_plant).appendChild(div_plant_info).appendChild(plant_name).appendChild(common_name);
             document.getElementById('plants').appendChild(div_plant).appendChild(div_plant_info).appendChild(plant_data).appendChild(plant_family);
             document.getElementById('plants').appendChild(div_plant).appendChild(div_plant_info).appendChild(plant_data).appendChild(line_break);
             document.getElementById('plants').appendChild(div_plant).appendChild(div_plant_info).appendChild(plant_data).appendChild(plant_genus);
-          }          
+            document.getElementById('plants').appendChild(div_plant).appendChild(div_plant_info).appendChild(plant_data).appendChild(more_button);
+          } // end loop
 
-        });
+        }); // end async function
+
+// new function triggerd by onCLick to get item detail info
+
+function plantDetail(){
+  document.getElementById('plants').innerHTML = ""; // remove contents from div id plants
+  console.log(more_info_link); // confirm it worked in the console
+
+  /*
+
+  ok, so dont forget to create a new call to a new endpoint and concatonate together a new URL and then build your DOM objects. 
+
+}
